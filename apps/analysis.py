bf1905da724c84240e3736f9156a086d697705ba
Andrew Ritchie <andrew@Andrews-MacBook-Air.local>
2020-09-15 20:34:00 +0100

    multipage app with basic CSS

---
+++ apps/analysis.py
@@ -0,0 +1,10 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+from dash.dependencies import Input, Output
+
+
+layout = html.Div([
+    html.H1('Analysis')
+])
--- index.py
+++ index.py
@@ -5,26 +5,25 @@ import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 from app import app
-from apps import app1, app2
+from apps import home, analysis, feed, about 
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 nav = dbc.Nav(
     [
-        dbc.NavItem(dbc.NavLink("Home", active=True, href="/apps/home")),
+        dbc.NavItem(dbc.NavLink("Home",  href="/apps/home")),
         dbc.NavItem(dbc.NavLink("Analysis", href="/apps/analysis")),
-        dbc.NavItem(dbc.NavLink("Feed", href="/apps/preparation")),
+        dbc.NavItem(dbc.NavLink("Feed", href="/apps/feed")),
         dbc.NavItem(dbc.NavLink("About", href="/apps/about"))
     ],
     horizontal=True,
-    fill=True
+    fill=True,
+    pills=True
 )
 SIDEBAR_STYLE = {
     "position": "fixed",
-    "height": '87%',
-    "left": 0,
-    "bottom": 0,
-    "width": "18%",
+    "padding": '10px',
+    "padding-bottom": '100%',
     "background-color": "#EAF0F1",
 }
 sidebar = html.Div(
@@ -37,7 +36,7 @@ sidebar = html.Div(
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(children=[html.H1('Nanoindentation Application', style={'color': 'blue', 'fontSize': 50, 'font-family': 'verdana', 'textAlign': 'center', 'backgroundColor': '#EAF0F1'}), nav], style={'backgroundColor': '#EAF0F1', 'height': '87%'}),
-    html.Div(sidebar),    
+    html.Div(children=[sidebar],style={'float':'left'}),    
     html.Div(id='page-content',style={"background-color": "#f8f9fa", 'textIndent':'50%'}),
     html.H5('test test test')
 ])
@@ -46,10 +45,14 @@ app.layout = html.Div([
 @app.callback(Output('page-content', 'children'),
                 [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/apps/app1':
-        return app1.layout
-    elif pathname == '/apps/app2':
-        return app2.layout
+    if pathname == '/apps/home':
+        return home.layout
+    elif pathname == '/apps/analysis':
+        return analysis.layout
+    elif pathname == '/apps/feed':
+        return feed.layout
+    elif pathname == '/apps/about':
+        return about.layout
     else:
         return '404'
 
